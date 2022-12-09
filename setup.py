f1431efc3999ff8544443b47d8e1710dd923e876
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2022-12-09 11:04:17 +0100

    .

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ from setuptools import setup
 setup(name='bit_IO',
       version='1.0',
       # list folders, not files
-      packages=['bit_IO'],
-      scripts=['make -C bit_IO/src/bit_IO/']
+      packages=['src/bit_IO'],
+      scripts=['scripts/compile']
       )
